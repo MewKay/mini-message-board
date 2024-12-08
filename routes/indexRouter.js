5ecd85bcf35e49881e2b053b970f958669f7a026
MewKay <miorakanto25z@gmail.com>
2024-12-08 12:41:25 +0300

    Add view to index

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,8 +2,21 @@ const { Router } = require("express");
 
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) => {
-  res.send("This is index");
+  res.render("index", { messages: messages });
 });
 
 module.exports = indexRouter;
