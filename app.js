c962e3c6a4073ab60c7bcc67c73fbe8746d90885
MewKay <miorakanto25z@gmail.com>
2024-12-08 12:54:33 +0300

    Add new message router

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const indexRouter = require("./routes/indexRouter");
+const newMessageRouter = require("./routes/newMessageRouter");
 app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
