5dbc983f2d40281f6d43abbe4c914cacbdbbc5ea
MewKay <miorakanto25z@gmail.com>
2024-12-23 11:58:49 +0300

    Change connection informations to connection string

--- db/populatedb.js
+++ db/populatedb.js
@@ -23,11 +23,7 @@ const main = async () => {
   console.log("seeding...");
 
   const client = new Client({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
   });
 
   await client.connect();
